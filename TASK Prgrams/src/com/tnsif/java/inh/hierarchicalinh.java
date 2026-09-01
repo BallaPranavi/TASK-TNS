e9ad1e4ec1f0ea6f1729b370ec0d1edbab7cdc92
BallaPranavi <ballapranavi8008@gmail.com>
2026-09-01 19:35:28 +0530

    Update HierarchicalIh.java

--- TASK Prgrams/src/com/tnsif/java/inh/hierarchicalinh.java
+++ TASK Prgrams/src/com/tnsif/java/inh/hierarchicalinh.java
@@ -1,10 +1,31 @@
 package com.tnsif.java.inh;
-
-public class hierarchicalinh {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+class Student {
+    public void studentDetails() {
+        String studentName = "Pranavi";
+        int studentAge = 21;
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Student Age: " + studentAge);
+    }
+}
+class Engineering extends Student {
+    public void engineeringDetails() {
+        String studentBranch = "IT";
+        System.out.println("Student Branch: " + studentBranch);
+    }
+}
+class Management extends Student {
+    public void managementDetails() {
+        String studentCourse = "BTech";
+        System.out.println("Student Course: " + studentCourse);
+    }
+}
+public class HierarchicalInh {
+    public static void main(String[] args) {
+        Engineering e = new Engineering();
+        e.studentDetails();
+        e.engineeringDetails();
+        Management m = new Management();
+        m.studentDetails();
+        m.managementDetails();
+    }
 }
